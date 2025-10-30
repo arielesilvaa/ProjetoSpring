11ee1b4a0791a2fdf7c40bb5c233c3b4a0857998
ariele <arielle.sil11@gmail.com>
2025-10-30 08:09:21 -0300

    subindo o projeto

---
+++ Backend/src/main/java/com/example/ProjetoInicial/PastaPrincipalApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoInicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastaPrincipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastaPrincipalApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/ProjetoInicial/PastaPrincipalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoInicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastaPrincipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
